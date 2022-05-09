44e9b024eab684b77c5750399823df9037e17637
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-05-09 15:34:32 +0530

    Added:Added Maximum Float

---
+++ MaximumFloatNumberTest.java
@@ -0,0 +1,30 @@
+package COM.BRIDGELABZ;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MaximumFloatNumberTest {
+
+    @Test
+        public void givenMaxNumberAtFirstPositionOrNot(){
+            MaximumFloatNumber maxNum = new MaximumFloatNumber();
+            double result = maxNum.MaxFloat(4.54);
+            Assert.assertEquals(1.29,result);
+
+        }
+        @Test
+        public void givenMaxNumberAtSecondPositionOrNot() {
+            MaximumFloatNumber maxNum = new MaximumFloatNumber();
+            double result = maxNum.MaxFloat(4.54);
+            Assert.assertEquals(3.56, result);
+
+        }
+        @Test
+        public void givenMaxNumberAtThirdPositionOrNot() {
+            MaximumFloatNumber maxNum = new MaximumFloatNumber();
+            double result = maxNum.MaxFloat(4.54);
+            Assert.assertEquals(4.54, result);
+
+        }
+
+    }
+
